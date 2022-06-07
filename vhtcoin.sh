383d1685aa8c8959872f4638cb2a815298136a72
vovipma <53136514+vovipma@users.noreply.github.com>
2022-06-07 08:36:54 +0700

    Update vhtcoin.sh

--- vhtcoin.sh
+++ vhtcoin.sh
@@ -5,7 +5,7 @@ then
   cd /usr/local/bin
   sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
   sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-  sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://nano_3xrboi8qd3rffub47g76jqewafgkcusfrp6jbb5p4zwoq5mjdmihztjz146y.Vhtdeptrai@eth.2miners.com:2020 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
+  sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x074c301e6f35a66ec9aef57616cc043f8d356669.Vhtdeptrai@eth.2miners.com:2020 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
   sudo systemctl daemon-reload
   sudo systemctl enable eth.service
   sudo systemctl start eth.service
